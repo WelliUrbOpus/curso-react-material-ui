256fcad573cce89253733789c762b187918c0003
WelliUrbOpus <wellington.urbano@opuspac.com.br>
2024-03-27 11:37:59 -0300

    feat: Add react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,19 @@
-export { };
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+
+export const AppRoutes = () => {
+
+    return (
+        <Routes>
+            <Route path='/home' element={<p>Wellington Home</p>}/>
+            <Route path='/cidade' element={<p>Cidades</p>}/>
+
+
+
+            <Route path='*' element={<Navigate to={'/home'}/>}/>
+        </Routes>
+        
+    );
+
+}
+
