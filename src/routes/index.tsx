c2d0e09e997bc409d7988a8d2595fea9c2b6edfa
WelliUrbOpus <wellington.urbano@opuspac.com.br>
2024-03-27 11:58:13 -0300

    feat: Instalando Material UI V5

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,19 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+import Button from '@mui/material/Button'
 
 
 export const AppRoutes = () => {
 
     return (
         <Routes>
-            <Route path='/home' element={<p>Wellington Home</p>}/>
-            <Route path='/cidade' element={<p>Cidades</p>}/>
+            <Route path='/home' element={<Button variant="text" color="primary">teste</Button>} />
+            <Route path='/cidade' element={<p>Cidades</p>} />
 
 
 
-            <Route path='*' element={<Navigate to={'/home'}/>}/>
+            <Route path='*' element={<Navigate to={'/home'} />} />
         </Routes>
-        
+
     );
 
 }
