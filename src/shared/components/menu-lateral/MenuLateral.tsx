02e4a3fe8e05ee168c598d2d2dcfc9c39859db4a
Wellington <wellington.urbano@opuspac.com.br>
2024-03-29 10:35:36 -0300

    feat: Criando menu lateral
    Criando menu lateral e add pack icon-material

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,4 +1,5 @@
-import { Drawer, Button, Box, useTheme, ButtonGroup, Avatar } from '@mui/material';
+import { Drawer, Button, Box, useTheme, Avatar, Divider, List, ListItemButton, ListItemText, ListItemIcon, Icon } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
 
 
 interface IMenuLateralProps {
@@ -12,9 +13,41 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
     <>
       <Drawer variant='permanent'>
 
-        <Box width={theme.spacing(28)}>
+        <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
 
-          <Avatar alt="Remy Sharp" src="" />
+          <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
+
+            <Avatar
+              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+              alt="Remy Sharp" src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png"
+            />
+
+          </Box>
+
+          <Divider />
+
+          <Box flex={1}>
+            <List component="nav" >
+
+              <ListItemButton >
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
+
+                <ListItemText primary="Home" />
+
+              </ListItemButton>
+
+              <ListItemButton>
+                <ListItemText primary="Btn 2" />
+              </ListItemButton>
+
+              <ListItemButton>
+                <ListItemText primary="Btn 3" />
+              </ListItemButton>
+
+            </List>
+          </Box>
 
         </Box>
 
