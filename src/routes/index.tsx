2b7352c4f9e70a2768996f2016dd1c7af8f62c72
Wellington <wellington.urbano@opuspac.com.br>
2024-03-31 06:28:27 -0300

    feat: Opções de menu no Drawer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,27 +3,42 @@ import Button from '@mui/material/Button';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useAppThemeContext, useDrawerContext } from '../shared/contexts';
 import { Box, useMediaQuery, useTheme } from '@mui/material';
+import { useEffect } from 'react';
 
 
 export const AppRoutes = () => {
   const { toggleTheme } = useAppThemeContext();
-  const { toggleDrawerOpen } = useDrawerContext();
+  const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
+  useEffect(() => {
+    setDrawerOptions([
+      {
+        path: '/home',
+        icon: 'Home',
+        label: 'Home'
+      },
+      {
+        path: '/cidades',
+        icon: 'Settings',
+        label: 'Cidades'
+      },
+      {
+        path: '/settings',
+        icon: 'Settings',
+        label: 'Settings'
+      }
+    ]);
+  });
 
   return (
     <Routes>
-      <Route path='/home' element={
-        <Button style={{ display: smDown ? 'inline-flex' : 'none' }} variant="contained" color="primary" onClick={toggleDrawerOpen}>
-          <MenuIcon />
-        </Button>}
-      />
+      <Route path='/home' element={<>Home</>} />
 
-      <Route path='/cidade' element={<p>Cidades</p>} />
+      <Route path='/settings' element={<>Configurações</>} />
 
-
-
+      <Route path='/cidades' element={<>Cidades</>} />
 
       <Route path='*' element={<Navigate to={'/home'} />} />
     </Routes>
--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,17 +1,58 @@
-import { Drawer, Button, Box, useTheme, Avatar, Divider, List, ListItemButton, ListItemText, ListItemIcon, Icon, useMediaQuery } from '@mui/material';
-import HomeIcon from '@mui/icons-material/Home';
+import { Drawer, Button, Box, useTheme, Avatar, Divider, List, ListItemButton, ListItemText, ListItemIcon, useMediaQuery } from '@mui/material';
+import { Home, Settings, Add, Menu } from '@mui/icons-material';
 import { useDrawerContext } from '../../contexts';
-
+import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
 
 interface IMenuLateralProps {
   children: React.ReactNode;
 }
 
+interface IListItemLinkProps {
+  to: string;
+  label: string;
+  icon: string;
+  onClick: (() => void) | undefined;
+}
+
+interface CustonIconProps {
+  tag: string;
+}
+
+const ListItemLink: React.FC<IListItemLinkProps> = ({ to, icon, label, onClick }) => {
+  const navigate = useNavigate();
+
+  const resolvedPath = useResolvedPath(to);
+  const match = useMatch({ path: resolvedPath.pathname, end: false });
+
+  const CustonIcon: React.FC<CustonIconProps> = ({ tag }) => {
+
+    if (tag === 'Home') return <Home />;
+    if (tag === 'Settings') return <Settings />;
+
+
+    return <Add />;
+  };
+
+  const handleClick = () => {
+    navigate(to);
+    onClick?.();
+  };
+
+  return (
+    <ListItemButton selected={!!match} onClick={handleClick}>
+      <ListItemIcon>
+        <CustonIcon tag={icon} />
+      </ListItemIcon>
+      <ListItemText primary={label} />
+    </ListItemButton>
+  );
+};
+
 export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
-  const {isDrawerOpen, toggleDrawerOpen} =useDrawerContext();
+  const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext();
 
   return (
     <>
@@ -32,23 +73,15 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
 
           <Box flex={1}>
             <List component="nav" >
-
-              <ListItemButton >
-                <ListItemIcon>
-                  <HomeIcon />
-                </ListItemIcon>
-
-                <ListItemText primary="Home" />
-
-              </ListItemButton>
-
-              <ListItemButton>
-                <ListItemText primary="Btn 2" />
-              </ListItemButton>
-
-              <ListItemButton>
-                <ListItemText primary="Btn 3" />
-              </ListItemButton>
+              {drawerOptions.map(drawerOptions => (
+                <ListItemLink
+                  to={drawerOptions.path}
+                  key={drawerOptions.path}
+                  icon={drawerOptions.icon}
+                  label={drawerOptions.label}
+                  onClick={smDown ? toggleDrawerOpen : undefined}
+                />
+              ))}
 
             </List>
           </Box>
@@ -57,7 +90,25 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
 
       </Drawer>
 
-      <Box height="100vh" marginLeft={smDown ? theme.spacing(1) : theme.spacing(28)}>
+      {/*<Drawer open={smDown} variant={'persistent'} >
+
+      <Box width={theme.spacing(10)} height={theme.spacing(10)} display="flex" flexDirection="column">
+        <Box width="100%" height={theme.spacing(5)} display="flex" alignItems="center" justifyContent="center">
+
+          <Button style={{ display: smDown ? 'inline-flex' : 'none' }} variant="contained" color="primary" onClick={toggleDrawerOpen}>
+            <Menu />
+          </Button>
+
+        </Box>
+      </Box>
+      </Drawer>*/}
+
+      <Box height="100vh" marginLeft={smDown ? theme.spacing(0) : theme.spacing(28)}>
+
+        <Button style={{ display: smDown ? 'inline-flex' : 'none' }} variant="contained" color="primary" onClick={toggleDrawerOpen}>
+          <Menu />
+        </Button>
+
 
         {children}
 
